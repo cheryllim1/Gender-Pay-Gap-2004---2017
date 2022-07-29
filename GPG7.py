ae2603a2f7de9d6c80d0fc28862a54108604a83f
cheryllim1 <109711152+cheryllim1@users.noreply.github.com>
2022-07-29 19:17:30 +0100

    Create GPG7.py

---
+++ GPG7.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+from tabulate import tabulate
+from sklearn.ensemble import RandomForestClassifier
+
+st.write("""
+# Gender Pay Gap
+This app predicts the Pay!
+""")
+
+st.sidebar.header('User Input Parameters')
+
+def user_input_features():
+  year = st.sidebar.slider('Year', 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017)
+  gender = st.sidebar.slider('Gender', female, male)
+   data = {'year': year,
+            'gender': gender}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+st.subheader('User Input parameters')
+st.write(df)
+
+clf = RandomForestClassifier()
+clf.fit(X, Y)
+
+prediction = clf.predict(df)
+prediction_proba = clf.predict_proba(df)
+
+st.subheader('Year and Gender')
+st.write(pay)
