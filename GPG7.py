03a8743ae08779502aac3c3306f6ebf493226db3
cheryllim1 <109711152+cheryllim1@users.noreply.github.com>
2022-07-29 19:37:20 +0100

    Update GPG7.py

--- GPG7.py
+++ GPG7.py
@@ -14,7 +14,7 @@ def user_input_features():
   year = st.sidebar.slider('Year', 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017)
   gender = st.sidebar.slider('Gender', female, male)
   data = {'year': year,'gender': gender}
-    features = pd.DataFrame(data, index=[0])
+  features = pd.DataFrame(data, index=[0])
     return features
 
 df = user_input_features()
